13b1159636182b06d08681a7dcbcb565bdd383c3
Young Seok Cho <yscho00172@gmail.com>
2021-04-11 01:46:28 +0900

    Update on Local Master Branch

--- main.py
+++ main.py
@@ -15,5 +15,8 @@ print("Hello World 4")
 # adding new item in master branch
 print("Hello World 5")
 
+# Updates in local master branch
+print("Hello World Updates on Local Master Branch")
+
 # Updates on mainbranch of GitHub
 print("Hello World Updates on GitHub")
